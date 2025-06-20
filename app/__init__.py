086cd2a4516af1c2d9b53674ef968ee6d1a46427
Kamal Koranga <kamalkoranga75@gmail.com>
2025-06-20 19:17:06 +0530

    Add error handling blueprints and templates for 404 and 500 errors
    Fix #12

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,9 @@ def create_app(config_class=Config):
     login.init_app(app)
     mail.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
